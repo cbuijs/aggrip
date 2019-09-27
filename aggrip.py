cd134a78c94fdfdbfbb06a22c08597778bc8b653
Chris Buijs <cbuijs@chrisbuijs.com>
2019-09-27 12:12:38 +0200

    Update 20190927-121238-CEST

--- aggrip.py
+++ aggrip.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
 =======================================================================
- aggrip.py v0.03-20190902 Copyright 2019 by cbuijs@chrisbuijs.com
+ aggrip.py v0.04-20190902 Copyright 2019 by cbuijs@chrisbuijs.com
 =======================================================================
 
  Aggregate IP list
@@ -15,22 +15,19 @@ import sys, socket
 # Regex
 import regex
 
-# Use module PyTricia for CIDR/Subnet stuff
-import pytricia
-
 # Use IPSet from IPy to aggregate
 from IPy import IP, IPSet
 
 # Lists
-lip4 = pytricia.PyTricia(32, socket.AF_INET)
-lip6 = pytricia.PyTricia(128, socket.AF_INET6)
+lip4 = set()
+lip6 = set()
 
 # IP Regexes
 ip_rx4 = '((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}(/(3[0-2]|[12]?[0-9]))*)'
 ip_rx6 = '(((:(:[0-9a-f]{1,4}){1,7}|::|[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,6}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,5}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,4}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,3}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,2}|::|:[0-9a-f]{1,4}(::[0-9a-f]{1,4}|::|:[0-9a-f]{1,4}(::|:[0-9a-f]{1,4}))))))))|(:(:[0-9a-f]{1,4}){0,5}|[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){0,4}|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){0,3}|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){0,2}|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4})?|:[0-9a-f]{1,4}(:|:[0-9a-f]{1,4})))))):(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})(/(12[0-8]|1[01][0-9]|[1-9]?[0-9]))*)'
 is_ip4 = regex.compile('^' + ip_rx4 + '$', regex.I)
 is_ip6 = regex.compile('^' + ip_rx6 + '$', regex.I)
-is_ip = regex.compile('^(' + ip_rx4 + '|' + ip_rx6 + ')$', regex.I)
+#is_ip = regex.compile('^(' + ip_rx4 + '|' + ip_rx6 + ')$', regex.I)
 
 ######################################################################
 
@@ -43,6 +40,7 @@ def agg(iplist):
 
     newlist = list()
     for ip in ipset:
+        ip.NoPrefixForSingleIp = None
         newlist.append(ip.strNormal(1))
 
     return newlist
@@ -54,9 +52,9 @@ if __name__ == '__main__':
     for line in sys.stdin:
         line = line.strip().lower()
         if is_ip4.search(line):
-            lip4[line] = line
+            lip4.add(line)
         elif is_ip6.search(line):
-            lip6[line] = line
+            lip6.add(line)
 
     for line in agg(lip4) + agg(lip6):
         sys.stdout.write(line + '\n')
